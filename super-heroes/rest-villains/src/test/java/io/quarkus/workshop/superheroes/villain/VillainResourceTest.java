ba03d5730b6958a311ab1c9eca57f1a5642d52af
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2023-05-31 12:18:16 -0500

    Villain section

--- super-heroes/rest-villains/src/test/java/io/quarkus/workshop/superheroes/villain/VillainResourceTest.java
+++ super-heroes/rest-villains/src/test/java/io/quarkus/workshop/superheroes/villain/VillainResourceTest.java
@@ -218,4 +218,13 @@ public class VillainResourceTest {
         };
     }
 
+    @Test
+    void shouldPingOpenAPI() {
+        given()
+            .header(ACCEPT, JSON)
+            .when().get("/q/openapi")
+            .then()
+            .statusCode(OK.getStatusCode());
+    }
+
 }
